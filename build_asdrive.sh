775290597489a16eb4e863149e837cd9e6dd9a94
hawk_hsieh <hawk_hsieh@miiicasa.com>
2015-10-04 18:56:40 -0700

    Add http_parser.c linkd_timer.c

--- build_asdrive.sh
+++ build_asdrive.sh
@@ -1,2 +1,2 @@
-rsync -av --exclude="asdrive/.git" --exclude="asdrive/soc" --exclude="asdrive/wolfssl" ../../../../asdrive ./extras/asdrive
+rsync -av --exclude=".git" --exclude="asdrive/soc" --exclude="asdrive/wolfssl" ../../../../asdrive ./extras/asdrive
 make -C ./demo_asdrive V=1
