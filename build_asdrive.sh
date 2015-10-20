2d07998a6cae985f913784f437bf9a36a5e27a41
hawk_hsieh <hawk_hsieh@miiicasa.com>
2015-10-19 22:40:39 -0700

    Duplicate a copied version from example/get_mbedtls and named demo_asdrive

--- build_asdrive.sh
+++ build_asdrive.sh
@@ -1,3 +1,4 @@
 rsync -av --exclude=".git" --exclude="SocketToRelay/soc/esp8266/esp-open-rtos" --exclude="SocketToRelay/soc/esp8266/esp-open-sdk" --exclude="SocketToRelay/wolfssl" ../../../../ ./extras/asdrive/asdrive
+sudo PATH=$PATH make flash -j4 -C ./examples/demo_asdrive ESPPORT=/dev/ttyUSB0
 #make -C ./demo_asdrive V=1
 make -C ./demo_asdrive $1
